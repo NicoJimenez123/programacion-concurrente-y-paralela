5cf399298654bc4361f957b803745348bc9d27d0
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2025-05-18 13:17:19 -0300

    Añadido GetByID y Create

--- technical-tests-backend-ssr/Controllers/ClienteController.cs
+++ technical-tests-backend-ssr/Controllers/ClienteController.cs
@@ -35,4 +35,33 @@ public class ClienteController : ControllerBase
     var clientes = await _clienteService.GetAllClientesAsync();
     return Ok(_mapper.Map<IEnumerable<ClienteDTO>>(clientes));
   }
+
+  /// <summary>
+  /// Obtener un cliente por su ID.
+  /// </summary>
+  /// <param name="id"></param>
+  /// <returns></returns>
+  [HttpGet("{id}")]
+  public async Task<ActionResult<ClienteDTO>> GetById(int id)
+  {
+    var cliente = await _clienteService.GetClienteByIdAsync(id);
+    if (cliente == null) return NotFound();
+    return Ok(_mapper.Map<ClienteDTO>(cliente));
+  }
+
+  /// <summary>
+  /// Agregar un nuevo cliente
+  /// </summary>
+  /// <param name="clienteDTO"></param>
+  /// <returns></returns>
+  [HttpPost]
+  public async Task<ActionResult<ClienteDTO>> Create(ClienteDTO clienteDTO)
+  {
+    // FluentValidation se hace automáticamente al verificar ModelState.IsValid.
+    if (!ModelState.IsValid) return BadRequest(ModelState);
+    // Mapear el DTO a la entidad Cliente y agregarlo a la base de datos.
+    var cliente = _mapper.Map<Cliente>(clienteDTO);
+    await _clienteService.AddClienteAsync(cliente);
+    return CreatedAtAction(nameof(GetById), new { id = cliente.Id }, _mapper.Map<ClienteDTO>(cliente));
+  }
 }
