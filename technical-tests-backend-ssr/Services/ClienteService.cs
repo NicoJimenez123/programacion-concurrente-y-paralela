fbeef128dcd3038ade0203bc9ab3bcb4251bf30e
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2025-05-18 13:29:41 -0300

    Implementados servicios faltantes de la Interfaz

--- technical-tests-backend-ssr/Services/ClienteService.cs
+++ technical-tests-backend-ssr/Services/ClienteService.cs
@@ -11,4 +11,34 @@ public class ClienteService {
   public Task<IEnumerable<Cliente>> GetAllClientesAsync() {
     return _clienteRepository.GetAllAsync();
   }
+
+  public Task<Cliente?> GetClienteByIdAsync(int id) {
+    return _clienteRepository.GetByIdAsync(id);
+  }
+
+  public async Task<Cliente> AddClienteAsync(Cliente cliente) {
+    await _clienteRepository.AddAsync(cliente);
+    return cliente;
+  }
+
+  public async Task<Cliente> UpdateClienteAsync(Cliente cliente) {
+    await _clienteRepository.UpdateAsync(cliente);
+    return cliente;
+  }
+
+  public async Task<bool> DeleteClienteAsync(int id) {
+    var existingCliente = await _clienteRepository.GetByIdAsync(id);
+    if (existingCliente == null) return false;
+
+    await _clienteRepository.DeleteAsync(id);
+    return true;
+  }
+  
+  public async Task<bool> DeleteClienteAsync(Cliente cliente) {
+    var existingCliente = await _clienteRepository.GetByIdAsync(cliente.Id);
+    if (existingCliente == null) return false;
+
+    await _clienteRepository.DeleteAsync(cliente.Id);
+    return true;
+  }
 }
