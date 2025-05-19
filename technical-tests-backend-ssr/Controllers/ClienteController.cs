c88ab5a49985a65cdb09d705ac0261acc10cf912
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2025-05-19 19:19:37 -0300

    Añadidas acciones Update y Delete

--- technical-tests-backend-ssr/Controllers/ClienteController.cs
+++ technical-tests-backend-ssr/Controllers/ClienteController.cs
@@ -64,4 +64,36 @@ public class ClienteController : ControllerBase
     await _clienteService.AddClienteAsync(cliente);
     return CreatedAtAction(nameof(GetById), new { id = cliente.Id }, _mapper.Map<ClienteDTO>(cliente));
   }
+
+  /// <summary>
+  /// Actualizar un cliente existente
+  /// </summary>
+  /// <param name="id"></param>
+  /// <param name="clienteDTO"></param>
+  /// <returns></returns>
+  [HttpPut("{id}")]
+  public async Task<ActionResult<ClienteDTO>> Update(int id, ClienteDTO clienteDTO)
+  {
+    if (id != clienteDTO.Id) return BadRequest("El ID del cliente no coincide.");
+    if (!ModelState.IsValid) return BadRequest(ModelState);
+    // Mapear el DTO a la entidad Cliente y actualizarlo en la base de datos.
+    var cliente = _mapper.Map<Cliente>(clienteDTO);
+    Console.WriteLine($"Actualizando cliente: {cliente.Id} en el controller con {id}");
+    await _clienteService.UpdateClienteAsync(cliente);
+    return Ok(_mapper.Map<ClienteDTO>(cliente));
+  }
+
+  /// <summary>
+  /// Eliminar un cliente por su ID
+  /// </summary>
+  /// <param name="id"></param>
+  /// <returns></returns>
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> Delete(int id)
+  {
+    var cliente = await _clienteService.GetClienteByIdAsync(id);
+    if (cliente == null) return NotFound();
+    await _clienteService.DeleteClienteAsync(cliente);
+    return NoContent();
+  }
 }
